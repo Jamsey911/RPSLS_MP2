5a81a1ddeeeb002f289fda05123c9c464d9d2ad4
Jamsey911 <jamesalynch911@hotmail.com>
2023-01-02 21:53:31 +0000

    Add JS for scoreboard

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,18 @@ const possibleChoices = document.querySelectorAll('button.selection')
 let userChoice
 let computerChoice
 
+// Increments Users Score
+function incrementScore() {
+    let oldScore = parseInt(document.getElementById('result-score').innerText)
+    document.getElementById('result-score').innerText = ++oldScore
+}
+// Increments Computers Score
+function incrementComputerScore() {
+    let oldScore = parseInt(document.getElementById('computer-result-score').innerText)
+    document.getElementById('computer-result-score').innerText = ++oldScore
+}
+
+// Add Click function for Game Button Selections
 possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('click', (e) => {
     userChoice = e.target.id
     userChoiceDisplay.innerHTML = userChoice
@@ -12,6 +24,7 @@ possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('cli
     getResult()
 }))
 
+// generate Computer Score
 function generateComputerChoice() {
     const randomNumber = Math.floor(Math.random() * possibleChoices.length)
 
@@ -33,6 +46,7 @@ function generateComputerChoice() {
     computerChoiceDisplay.innerHTML = computerChoice
 }
 
+// Generate Result
 function getResult() {
     // Draw funtion
     if (computerChoice === userChoice) {
@@ -40,65 +54,85 @@ function getResult() {
     }
     // Lose Funtion
     if (computerChoice === 'rock' && userChoice === "scissors") {
-        result = 'You Lose!'
+        result = 'You Lose!';
+        incrementComputerScore();
     }
     if (computerChoice === 'rock' && userChoice === "lizard") {
-        result = 'You Lose!'
+        result = 'You Lose!';
+        incrementComputerScore();
     }
     if (computerChoice === 'paper' && userChoice === 'rock') {
-        result = 'You Lose!'
+        result = 'You Lose!';
+        incrementComputerScore();
     }
     if (computerChoice === 'paper' && userChoice === 'spock') {
-        result = 'You Lose!'
+        result = 'You Lose!';
+        incrementComputerScore();
     }
     if (computerChoice === 'scissors' && userChoice === 'paper') {
-        result = 'You Lose!'
+        result = 'You Lose!';
+        incrementComputerScore();
     }
     if (computerChoice === 'scissors' && userChoice === 'lizard') {
-        result = 'You Lose!'
+        result = 'You Lose!';
+        incrementComputerScore();
     }
     if (computerChoice === 'lizard' && userChoice === 'paper') {
-        result = 'You Lose!'
+        result = 'You Lose!';
+        incrementComputerScore();
     }
     if (computerChoice === 'lizard' && userChoice === 'spock') {
-        result = 'You Lose!'
+        result = 'You Lose!';
+        incrementComputerScore();
     }
     if (computerChoice === 'spock' && userChoice === 'scissors') {
-        result = 'You Lose!'
+        result = 'You Lose!';
+        incrementComputerScore();
     }
     if (computerChoice === 'spock' && userChoice === 'rock') {
-        result = 'You Lose!'
+        result = 'You Lose!';
+        incrementComputerScore();
     }
     // Win Function
     if (computerChoice === 'scissors' && userChoice === 'rock') {
-        result = 'You Win!'
+        result = 'You Win!';
+        incrementScore();
     }
     if (computerChoice === 'lizard' && userChoice === 'rock') {
-        result = 'You Win!'
+        result = 'You Win!';
+        incrementScore();
     }
     if (computerChoice === 'scissors' && userChoice === 'paper') {
         result = 'You Win!'
+        incrementScore();
     }
     if (computerChoice === 'spock' && userChoice === 'paper') {
         result = 'You Win!'
+        incrementScore();
     }
     if (computerChoice === 'paper' && userChoice === 'scissors') {
         result = 'You Win!'
+        incrementScore();
     }
     if (computerChoice === 'lizard' && userChoice === 'scissors') {
         result = 'You Win!'
+        incrementScore();
     }
     if (computerChoice === 'paper' && userChoice === 'lizard') {
-        result = 'You Win!'
+        result = 'You Win!';
+        incrementScore();
     }
     if (computerChoice === 'spock' && userChoice === 'lizard') {
-        result = 'You Win!'
+        result = 'You Win!';
+        incrementScore();
     }
     if (computerChoice === 'scissors' && userChoice === 'spock') {
-        result = 'You Win!'
+        result = 'You Win!';
+        incrementScore();
     }
     if (computerChoice === 'rock' && userChoice === 'spock') {
-        result = 'You Win!'
+        result = 'You Win!';
+        incrementScore();
     }
     resultDisplay.innerHTML = result
 }
