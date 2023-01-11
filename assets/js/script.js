7cb571107e900084b960376423fd9e11fc7849ae
Jamsey911 <jamesalynch911@hotmail.com>
2023-01-11 21:52:07 +0000

    Clean code

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,6 +17,9 @@ let clear = document.querySelector("#selections");
  */
 function bestOfThree() {
   if (userNewScore == 3) {
+    /**
+     * Audio plays when user wins three games
+     */
     let win = new Audio("assets/audio/win.mp3");
     win.play();
         /**
@@ -25,6 +28,9 @@ function bestOfThree() {
     setTimeout(function () {
     location.reload();
     }, 7000); 
+    /**
+     * Changes selections display to the below if user wins three games
+     */
     clear.style.backgroundColor = "green";
     clear.style.fontSize = '2rem';
     clear.style.padding = '20px 0';
@@ -33,14 +39,20 @@ function bestOfThree() {
     clear.innerHTML = ('Well done, you won!  (The Game will restart shortly. Have another go!)');
        
   } if (compNewScore == 3) {
+    /**
+     * Audio loses when user wins three games
+     */
     let lose = new Audio("assets/audio/lose.mp3");
     lose.play();
     /**
      * resets game after 7 seconds once winner is declared
      */
-  setTimeout(function () {
+    setTimeout(function () {
     location.reload();
     }, 7000);
+    /**
+     * Changes selections display to the below if user loses three games
+     */
     clear.style.backgroundColor = "red";
     clear.style.fontSize = '2rem';
     clear.style.padding = '20px 0';
@@ -93,104 +105,104 @@ function generateComputerChoice() {
   if (randomNumber === 4) {
     computerChoice = 'Spock🖖';
   }
-computerChoiceDisplay.innerHTML = computerChoice;
+  computerChoiceDisplay.innerHTML = computerChoice;
 }
 
 /**
  * Generate Result
  */
 function getResult() {
-    /**
-     * Draw funtion
-     */
-    if (computerChoice === userChoice) {
-        result = 'Its A Draw';
-    }
-    /**
-     * Lose Funtion
-     */
-    if (computerChoice === 'Rock👊' && userChoice === "Scissors✂️") {
-        result = 'You Lose!';
-        incrementComputerScore();
-    }
-    if (computerChoice === 'Rock👊' && userChoice === "Lizard🦎") {
-        result = 'You Lose!';
-        incrementComputerScore();
-    }
-    if (computerChoice === 'Paper📃' && userChoice === 'Rock👊') {
-        result = 'You Lose!';
-        incrementComputerScore();
-    }
-    if (computerChoice === 'Paper📃' && userChoice === 'Spock🖖') {
-        result = 'You Lose!';
-        incrementComputerScore();
-    }
-    if (computerChoice === 'Scissors✂️' && userChoice === 'Paper📃') {
-        result = 'You Lose!';
-        incrementComputerScore();
-    }
-    if (computerChoice === 'Scissors✂️' && userChoice === 'Lizard🦎') {
-        result = 'You Lose!';
-        incrementComputerScore();
-    }
-    if (computerChoice === 'Lizard🦎' && userChoice === 'Paper📃') {
-        result = 'You Lose!';
-        incrementComputerScore();
-    }
-    if (computerChoice === 'Lizard🦎' && userChoice === 'Spock🖖') {
-        result = 'You Lose!';
-        incrementComputerScore();
-    }
-    if (computerChoice === 'Spock🖖' && userChoice === 'Scissors✂️') {
-        result = 'You Lose!';
-        incrementComputerScore();
-    }
-    if (computerChoice === 'Spock🖖' && userChoice === 'Rock👊') {
-        result = 'You Lose!';
-        incrementComputerScore();
-    }
-    /**
-     * Win Function
-     */
-    if (computerChoice === 'Scissors✂️' && userChoice === 'Rock👊') {
-        result = 'You Win!';
-        incrementScore();
-    }
-    if (computerChoice === 'Lizard🦎' && userChoice === 'Rock👊') {
-        result = 'You Win!';
-        incrementScore();
-    }
-    if (computerChoice === 'Rock👊' && userChoice === 'Paper📃') {
-        result = 'You Win!';
-        incrementScore();
-    }
-    if (computerChoice === 'Spock🖖' && userChoice === 'Paper📃') {
-        result = 'You Win!';
-        incrementScore();
-    }
-    if (computerChoice === 'Paper📃' && userChoice === 'Scissors✂️') {
-        result = 'You Win!';
-        incrementScore();
-    }
-    if (computerChoice === 'Lizard🦎' && userChoice === 'Scissors✂️') {
-        result = 'You Win!';
-        incrementScore();
-    }
-    if (computerChoice === 'Paper📃' && userChoice === 'Lizard🦎') {
-        result = 'You Win!';
-        incrementScore();
-    }
-    if (computerChoice === 'Spock🖖' && userChoice === 'Lizard🦎') {
-        result = 'You Win!';
-        incrementScore();
-    }
-    if (computerChoice === 'Scissors✂️' && userChoice === 'Spock🖖') {
-        result = 'You Win!';
-        incrementScore();
-    }
-    if (computerChoice === 'Rock👊' && userChoice === 'Spock🖖') {
-        result = 'You Win!';
-        incrementScore();
-    }
-    resultDisplay.innerHTML = result;
+  /**
+   * Draw funtion
+   */
+  if (computerChoice === userChoice) {
+      result = 'Its A Draw';
+  }
+  /**
+   * Lose Funtion
+   */
+  if (computerChoice === 'Rock👊' && userChoice === "Scissors✂️") {
+      result = 'You Lose!';
+      incrementComputerScore();
+  }
+  if (computerChoice === 'Rock👊' && userChoice === "Lizard🦎") {
+      result = 'You Lose!';
+      incrementComputerScore();
+  }
+  if (computerChoice === 'Paper📃' && userChoice === 'Rock👊') {
+      result = 'You Lose!';
+      incrementComputerScore();
+  }
+  if (computerChoice === 'Paper📃' && userChoice === 'Spock🖖') {
+      result = 'You Lose!';
+      incrementComputerScore();
+  }
+  if (computerChoice === 'Scissors✂️' && userChoice === 'Paper📃') {
+      result = 'You Lose!';
+      incrementComputerScore();
+  }
+  if (computerChoice === 'Scissors✂️' && userChoice === 'Lizard🦎') {
+      result = 'You Lose!';
+      incrementComputerScore();
+  }
+  if (computerChoice === 'Lizard🦎' && userChoice === 'Paper📃') {
+      result = 'You Lose!';
+      incrementComputerScore();
+  }
+  if (computerChoice === 'Lizard🦎' && userChoice === 'Spock🖖') {
+      result = 'You Lose!';
+      incrementComputerScore();
+  }
+  if (computerChoice === 'Spock🖖' && userChoice === 'Scissors✂️') {
+      result = 'You Lose!';
+      incrementComputerScore();
+  }
+  if (computerChoice === 'Spock🖖' && userChoice === 'Rock👊') {
+      result = 'You Lose!';
+      incrementComputerScore();
+  }
+  /**
+   * Win Function
+   */
+  if (computerChoice === 'Scissors✂️' && userChoice === 'Rock👊') {
+      result = 'You Win!';
+      incrementScore();
+  }
+  if (computerChoice === 'Lizard🦎' && userChoice === 'Rock👊') {
+      result = 'You Win!';
+      incrementScore();
+  }
+  if (computerChoice === 'Rock👊' && userChoice === 'Paper📃') {
+      result = 'You Win!';
+      incrementScore();
+  }
+  if (computerChoice === 'Spock🖖' && userChoice === 'Paper📃') {
+      result = 'You Win!';
+      incrementScore();
+  }
+  if (computerChoice === 'Paper📃' && userChoice === 'Scissors✂️') {
+      result = 'You Win!';
+      incrementScore();
+  }
+  if (computerChoice === 'Lizard🦎' && userChoice === 'Scissors✂️') {
+      result = 'You Win!';
+      incrementScore();
+  }
+  if (computerChoice === 'Paper📃' && userChoice === 'Lizard🦎') {
+      result = 'You Win!';
+      incrementScore();
+  }
+  if (computerChoice === 'Spock🖖' && userChoice === 'Lizard🦎') {
+      result = 'You Win!';
+      incrementScore();
+  }
+  if (computerChoice === 'Scissors✂️' && userChoice === 'Spock🖖') {
+      result = 'You Win!';
+      incrementScore();
+  }
+  if (computerChoice === 'Rock👊' && userChoice === 'Spock🖖') {
+      result = 'You Win!';
+      incrementScore();
+  }
+  resultDisplay.innerHTML = result;
 }
